c9cbff5e1dce74ca45b01ce96d58647ff6401650
vananh0909 <anhb2014814@student.ctu.edu.vn>
2023-09-23 00:08:29 +0700

    Cai dat express chay thong bao xin chao

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const cors = require("cors");
+// const contactsRouter = require("./app/routes/contact.route");
+
+
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.json({ message: "Welcome to contact book application."});
+
+
+app.get("/",(req,res) => {
+    res.json({ message: "Welcome to contact book application"});
 });
+
+
 module.exports = app;
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const app = require("./app");
 const config = require("./app/config");
 
-// start server 
-const PORT = config.app.port;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
-});
+
+
+        const PORT = config.app.port;
+        app.listen(PORT, ()=>{
+            console.log(`Server is running on port ${PORT}`);
+        });
+    
