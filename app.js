312d286cd526d4e828cf90bfeac5fa73754993d3
vananh0909 <anhb2014814@student.ctu.edu.vn>
2023-09-23 00:44:07 +0700

    Dinh nghia cac route cho tai nguyen contact

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const cors = require("cors");
-// const contactsRouter = require("./app/routes/contact.route");
+const contactsRouter = require("./app/routes/contact.route");
 
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use("/api/contacts", contactsRouter);
 
 
 app.get("/",(req,res) => {
